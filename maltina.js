58e20a04f64e64126cc0157d7b95ad80c0876c45
King Akidi <onlinemraam@gmail.com>
2020-10-30 14:32:24 +0100

    REFRACTOR JS FILE WITH FUNCTIONS CALL

--- maltina.js
+++ maltina.js
@@ -1,35 +1,21 @@
-// setTimeout(() => {
-//   document.getElementById("body").style.backgroundColor = "red";
-// }, 5000);
-// let red = Math.floor(Math.random() * 256);
-// let green = Math.floor(Math.random() * 256);
-// let blue = Math.floor(Math.random() * 256);
+function containerBox(color) {
+  return `<div style="background-color:${color}"></div>`;
+}
+function showContainer(color) {
+  let container = document.getElementById("container");
+  container.innerHTML += containerBox(color);
+}
 
-// setTimeout(() => {
-//   document.getElementById("malt").style.backgroundColor = colors(
-//     red,
-//     green,
-//     blue
-//   );
-// }, 2000);
-
-// let red, green, blue;
-
-// document.write(red, "  ", green, " ", blue);
-
-// function colors(red, green, blue) {
-//   return `rgb(${red}, ${green}, ${blue})`;
-// }
-
-setInterval(() => {
+// SHOW FUNCTION CONTAINING BOX COLORS AND CONTAINER
+function ShowBox() {
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
   let color = `rgb(${red}, ${green}, ${blue})`;
-  let malt = document.getElementById("malt");
-  let container = document.getElementById("container");
-  //   malt.style.backgroundColor = color;
-  container.innerHTML =
-    container.innerHTML + `<div style="background-color:${color}"></div>`;
-  console.log(container.innerHTML);
+  containerBox(color);
+  showContainer(color);
+}
+
+setInterval(() => {
+  ShowBox();
 }, 2000);
